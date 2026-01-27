f1f8d5ede00f80329c1b5190810714bb064cf7fd
Jawwad926 <mdsabbirhosen926@gmail.com>
2026-01-28 04:56:17 +0600

    feat: create Post entity and register with TypeORM

---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  title: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column()
+  authorName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
