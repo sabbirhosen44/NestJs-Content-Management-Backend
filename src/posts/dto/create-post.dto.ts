951cb3b4926964a4f1e7724f605c2485f2e2d970
Jawwad926 <mdsabbirhosen926@gmail.com>
2026-01-27 10:52:13 +0600

    feat: implement data validation for posts module
    - Add DTOs for create and update operations
    - Add custom validation pipes for post data
    - Configure validation rules and transformations

---
+++ src/posts/dto/create-post.dto.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty({ message: 'Title is required' })
+  @IsString({ message: 'Title must be a string' })
+  @MinLength(3, { message: 'Title must be atleast 3 characters long' })
+  @MaxLength(50, { message: 'Title can not be longer than 50 characters' })
+  title: string;
+
+  @IsNotEmpty({ message: 'Content is required' })
+  @IsString({ message: 'Content must be a string' })
+  @MinLength(50, { message: 'Content must be atleast 50 characters long' })
+  content: string;
+
+  @IsNotEmpty({ message: 'Author is required' })
+  @IsString({ message: 'Author must be a string' })
+  @MinLength(2, { message: 'Author must be atleast 2 characters long' })
+  @MaxLength(25, { message: 'Author can not be longer than 25 characters' })
+  authorName: string;
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -11,8 +11,11 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
 import type { Post as PostInterface } from './interfaces/post.interface';
+import { PostsService } from './posts.service';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PostExistsPipe } from './pipes/post-exists.pipe';
 
 @Controller('posts')
 export class PostsController {
@@ -32,29 +35,29 @@ export class PostsController {
   }
 
   @Get(':id')
-  getSinglePost(@Param('id', ParseIntPipe) id: number): PostInterface {
+  getSinglePost(
+    @Param('id', ParseIntPipe, PostExistsPipe) id: number,
+  ): PostInterface {
     return this.postsService.getSinglePost(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  createPost(
-    @Body() newPostData: Omit<PostInterface, 'id' | 'createdAt'>,
-  ): PostInterface {
+  createPost(@Body() newPostData: CreatePostDto): PostInterface {
     return this.postsService.createPost(newPostData);
   }
 
   @Put(':id')
   updatePost(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updatePostData: Partial<Omit<PostInterface, 'id' | 'createdAt'>>,
+    @Param('id', ParseIntPipe, PostExistsPipe) id: number,
+    @Body() updatePostData: UpdatePostDto,
   ): PostInterface {
     return this.postsService.updatePost(id, updatePostData);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  deletePost(@Param('id', ParseIntPipe) id: number): void {
+  deletePost(@Param('id', ParseIntPipe, PostExistsPipe) id: number): void {
     this.postsService.deletePost(id);
   }
 }
