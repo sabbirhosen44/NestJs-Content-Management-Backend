b7b4a70ea673558d52d44ec21da8a263053daba2
Jawwad926 <mdsabbirhosen926@gmail.com>
2026-01-28 04:31:40 +0600

    feat: configure TypeORM with Neon PostgreSQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('NODE_ENV') === 'development',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
